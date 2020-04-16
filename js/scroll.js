5c833796b43c3c0cfbb02a850f63435bf4deed49
lotostoi <lotos_toi@mail.ru>
2020-04-17 02:44:24 +1000

    start_adaptiv

--- js/scroll.js
+++ js/scroll.js
@@ -23,3 +23,22 @@ arrPages.forEach(e => {
         e.style.minHeight = heightW + 'px'
     }
 })
+
+let on = document.querySelector('.fa-bars')
+let off = document.querySelector('.fa-times')
+
+if (document.querySelector('.menu__ofOn')) {
+
+    on.addEventListener('click', (e) => {
+        on.style.display = 'none'
+        off.style.display = 'block'
+        document.querySelector('.menu__ul').classList.add('menu__ul-active')
+    })
+    off.addEventListener('click', (e) => {
+        on.style.display = 'block'
+        off.style.display = 'none'
+        document.querySelector('.menu__ul').classList.remove('menu__ul-active')
+    })
+
+
+}
